98ba6c0c4243a1d2a21b28831eb4dcb41907cb41
zahraG1989 <93261667+zahraG1989@users.noreply.github.com>
2022-12-16 21:07:55 +0100

    foo

---
+++ Application.Test/UploadServiceTest.cs
@@ -0,0 +1,57 @@
+﻿using Application.Models;
+using Application.Services;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+using static Xunit.Assert;
+namespace Application.Test;
+
+public class UploadServiceTest
+{
+    private UploadService _service;
+    private Mock<IFormFile> _fromFileMock;
+
+    public UploadServiceTest()
+    {
+        _service = new UploadService();
+        _fromFileMock = new Mock<IFormFile>();
+    }
+
+    [Fact]
+    public async Task ShouldGetFilePathAfterSavingFileIntoWebRootPath()
+    {
+        var fileName = "file1";
+        _fromFileMock.SetupGet(file => file.FileName).Returns(fileName+".jpg");
+        var request = GetSampleRequest();
+        await _service.SavePostImageAsync(request, "");
+        var expectedPath = Path.Combine("", "images", "products", fileName);
+        True(request.ImagePath.StartsWith(expectedPath));
+    }
+
+    [Fact]
+    public async Task ShouldCopyUploadedImageToServerLocation()
+    {
+        var request = GetSampleRequest();
+        await _service.SavePostImageAsync(request, "");
+        _fromFileMock.Verify(file => file.CopyToAsync(
+            It.IsAny<FileStream>(), CancellationToken.None),Times.Once());
+    }
+    [Fact]
+    public async Task ShouldGetSuccessAndCombinedImagePathWhenCreatingPost()
+    {
+        var request = GetSampleRequest();
+        request.ImagePath = "file1";
+        var response = await _service.CreatePostAsync(request);
+        True(response.Success);
+        Equal(response.ImagePath,Path.Combine("images","products",request.ImagePath));
+    }
+
+    private UploadRequest GetSampleRequest()
+    {
+        return new UploadRequest
+        {
+            Image = _fromFileMock.Object,
+            ImagePath = ""
+        };
+    }
+}
