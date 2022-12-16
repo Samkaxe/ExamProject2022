0ac217f89893b19ab1100346a0f625f619cece1e
zahraG1989 <93261667+zahraG1989@users.noreply.github.com>
2022-12-16 20:53:10 +0100

    testing

--- Application/Services/UploadService.cs
+++ Application/Services/UploadService.cs
@@ -9,18 +9,27 @@ public class UploadService : IUploadService
 {
     public async Task SavePostImageAsync(UploadRequest uploadRequest, string environmentWebRootPath)
     {
-        var uniqueFileName = FileHelper.GetUniqueFileName(uploadRequest.Image.FileName);
-
-        var uploads = Path.Combine(environmentWebRootPath, "images", "products");
-        var filePath = Path.Combine(uploads, uniqueFileName);
-        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+        var filePath = GetFilePath(environmentWebRootPath, uploadRequest.Image.FileName);
         await uploadRequest.Image.CopyToAsync(new FileStream(filePath, FileMode.Create));
         uploadRequest.ImagePath = filePath;
     }
+
+    
 
     public async Task<UploadResponse> CreatePostAsync(UploadRequest postRequest)
     {
         return new UploadResponse
             {Success = true, ImagePath = Path.Combine("images", "products", Path.GetFileName(postRequest.ImagePath))};
+    }
+    
+    
+    
+    private static string GetFilePath(string environmentWebRootPath, string imageFileName)
+    {
+        var uniqueFileName = FileHelper.GetUniqueFileName(imageFileName);
+        var uploads = Path.Combine(environmentWebRootPath, "images", "products");
+        var filePath = Path.Combine(uploads, uniqueFileName);
+        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+        return filePath;
     }
 }
