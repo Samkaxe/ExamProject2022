ba91e9e8cbe11d126b24e349c3c009f9eed906a7
Samkaxe <90753108+Samkaxe@users.noreply.github.com>
2022-12-26 14:04:29 +0100

    TokenService

---
+++ Application/Interfaces/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using Core.Identity;
+
+namespace Application.Interfaces;
+
+public interface ITokenService
+{
+    string CreateToken(AppUser user);
+}
---
+++ Application/Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Application.Interfaces;
+using Core.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application.Services;
+
+public class TokenService : ITokenService
+{
+    private readonly IConfiguration _config;
+    private readonly SymmetricSecurityKey _key;
+
+    public TokenService(IConfiguration config)
+    {
+        _config = config;
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Token:Key"]));
+    }
+
+    public string CreateToken(AppUser user)
+    {
+        // this token should contain some data from the user info 
+        
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.GivenName, user.DisplayName)
+        };
+            // then we crypt the claims 
+        var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            // create token with the informantion provided 
+        var token = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds,
+            Issuer = _config["Token:Issuer"]
+        };
+
+        var jwtToken = new JwtSecurityTokenHandler();
+
+        var tken = jwtToken.CreateToken(token);
+
+        return jwtToken.WriteToken(tken);
+    }
+}
