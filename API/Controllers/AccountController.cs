24f387ab94deb00b10a1f7616d24fa657aa79c45
Samkaxe <90753108+Samkaxe@users.noreply.github.com>
2022-12-30 14:25:36 +0100

    refactor

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -46,38 +46,7 @@ public class AccountController : BaseApiController
         return await _userManager.FindByEmailAsync(email) != null;
     }
 
-    // [HttpGet("adresss")]
-    // public async Task<ActionResult<Address>> GetUserAddress()
-    // {
-    //     return null;
-    // } 
-    
-    /*
-* {
-"errors": [
-   "The Email field is not a valid e-mail address.",
-   "The Password field is required."
-],
-"statusCode": 400,
-"message": "A bad request , you have made"
-}
-*/
-    
-    /*
-     *  "errors": [
-        "Email address is in use"
-    ],
-    "statusCode": 400,
-    "message": "A bad request , you have made"
-}
-     */
-    // Attempts a password sign in for a user.
-    //     Params:
-    // user – The user to sign in.
-    // password – The password to attempt to sign in with.
-    //     lockoutOnFailure – Flag indicating if the user account should be locked if the sign in fails.
-    //     Returns:
-    // The task object representing the asynchronous operation containing the for the sign-in attempt.
+   
 
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
@@ -127,31 +96,5 @@ public class AccountController : BaseApiController
     
    
 
-    /*
-    * "email": "Alex@test.com",
-   "displayName": "Alex",
-   "token": "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkFsZXhAdGVzdC5jb20iLCJnaXZlbl9uYW1
-   lIjoiQWxleCIsIm5iZiI6MTY2NTk1ODExOSwiZXhwIjoxNjY2NTYyOTE5LCJpYXQiOjE2NjU5NTgxMTksImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDEifQ.JIf2DixvHqPdnLLBgEDM3YhorhKIHsVdkKqFeImrM0iCIKfcYA9xwoOZx_APbovW8QPzaqwqhgKqCeyZGBJ1xQ"
-   {
- "alg": "HS512",
- "typ": "JWT"
- }
-  {
-        "email": "Alex@test.com",
-        "given_name": "Alex",
-        "nbf": 1665958119,
-        "exp": 1666562919,
-        "iat": 1665958119,
-        "iss": "https://localhost:5001"
-  }
-
-           HMACSHA512(
-           base64UrlEncode(header) + "." +
-           base64UrlEncode(payload),
-           
-         your-256-bit-secret
-
-          ) secret base64 encoded
-   
-      */
+ 
 }
